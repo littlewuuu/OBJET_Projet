628a1e8355a3e10b34a67bbab53bf3844ed5f736
littlewuuu <2622632937@qq.com>
2022-09-20 12:50:32 +0200

    tp3-09/20

--- src/main/java/org/centrale/objet/WoE/Monstre.java
+++ src/main/java/org/centrale/objet/WoE/Monstre.java
@@ -8,30 +8,49 @@ package org.centrale.objet.WoE;
 import java.util.Random;
 
 /**
+ * The class Monstre represents the monsters of the world, a class that inherits
+ * from the Creature class.
  *
- * @author wuzilong et zoukang
+ * @author WuZilong
+ * @author ZouKang
+ * @version 1.0
  */
 public class Monstre extends Creature {
 
+    /**
+     * Initializes a newly created Monstre object, so that it represents
+     * the same values of characters as the argument.
+     * @param ptVie
+     * @param degAtt
+     * @param ptPar            
+     * @param pageAtt
+     * @param pagePar
+     * @param pos 
+     */
     public Monstre(int ptVie, int degAtt, int ptPar, int pageAtt, int pagePar, Point2D pos) {
         super(ptVie, degAtt, ptPar, pageAtt, pagePar, pos);
     }
-
+    /**
+     * Initializes a newly created Monstre object by another Monstre objet m
+     * @param m 
+     */
     public Monstre(Creature m) {
         super(m);
     }
 
+    /**
+     * Initializes a newly created Monstre object so that all the attributes are 0 or null.
+     */
     public Monstre() {
+        super();
     }
-   
     
-
-    
-    public void affiche(){
+    /**
+     * To display all information of a Monstre objet.
+     */
+    public void affiche() {
         super.affiche();
     }
 
-
-    
 
 }
